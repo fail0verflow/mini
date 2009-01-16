f0edf39096632756aa82133324db14b07aba4f1c
marcan <marcan@marcansoft.com>
2009-01-16 08:46:36 +0100

    Mark gecko_printf as printf-style for arg checking

--- gecko.h
+++ gecko.h
@@ -12,7 +12,7 @@ int gecko_sendbuffer_safe(const void *buffer, u32 size);
 int gecko_putchar(int c);
 int gecko_getchar(void);
 int gecko_puts(const char *s);
-int gecko_printf( const char *fmt, ...);
+int gecko_printf( const char *fmt, ...) __attribute__((format (printf, 1, 2)));
 void gecko_init(void);
 
 #endif
