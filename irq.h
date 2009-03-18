c8c8d92787537a94eb58f1a0f040ad32d8efd184
marcan <marcan@marcansoft.com>
2009-03-18 18:15:19 +0100

    Add more IRQ numbers

--- irq.h
+++ irq.h
@@ -3,15 +3,22 @@
 
 #define IRQ_TIMER	0
 #define IRQ_NAND	1
-#define	IRQ_AES		2	// not sure on this one
+#define IRQ_AES		2
+#define IRQ_SHA1	3
+#define IRQ_EHCI	4
+#define IRQ_OHCI0	5
+#define IRQ_OHCI1	6
+#define IRQ_SDHC	7
+#define IRQ_WIFI	8
 #define IRQ_GPIO1B	10
 #define IRQ_GPIO1	11
 #define IRQ_RESET	17
+#define IRQ_PPCIPC	30
 #define IRQ_IPC		31
 
 #define IRQF_TIMER	(1<<IRQ_TIMER)
 #define IRQF_NAND	(1<<IRQ_NAND)
-#define	IRQF_AES	(1<<IRQ_AES)
+#define IRQF_AES	(1<<IRQ_AES)
 #define IRQF_GPIO1B	(1<<IRQ_GPIO1B)
 #define IRQF_GPIO1	(1<<IRQ_GPIO1)
 #define IRQF_RESET	(1<<IRQ_RESET)
