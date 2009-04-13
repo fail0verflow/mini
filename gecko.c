c4699bda338b7cfd744e4a56e543cedef153345d
dhewg <dhewg@wiibrew.org>
2009-04-13 23:38:37 +0200

    aaaand some more license crap

--- gecko.c
+++ gecko.c
@@ -3,6 +3,7 @@
 
 	boot2 chainloader
 
+Copyright (c) 2008			Nuke - <wiinuke@gmail.com>
 Copyright (C) 2008, 2009	Hector Martin "marcan" <marcan@marcansoft.com>
 Copyright (C) 2008, 2009	Sven Peter <svenpeter@gmail.com>
 Copyright (C) 2009			Andre Heider "dhewg" <dhewg@wiibrew.org>
--- sdmmc.c
+++ sdmmc.c
@@ -3,7 +3,7 @@
 
 	SD/MMC interface
 
-Copyright (C) 2008, 2009 	Sven Peter <svenpeter@gmail.com>
+Copyright (C) 2008, 2009	Sven Peter <svenpeter@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
