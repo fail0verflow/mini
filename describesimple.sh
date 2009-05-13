a019678827a352d5a62c44bb03859dcfd10b383c
Sven Peter <svenpeter@gmail.com>
2009-05-13 21:39:57 +0200

    wtf

--- describesimple.sh
+++ describesimple.sh
@@ -3,11 +3,7 @@
 # Check for git and a git repo.
 if head=`git rev-parse --verify HEAD 2>/dev/null`; then
 	comm=`git log --pretty=oneline -n1 | awk ' { print $1 }'`
-	if tagver=`git describe --exact-match --match bootmii-\* $comm 2>/dev/null`; then
-		printf "%s" "BootMii v"`printf "%s" $tagver | awk '{ sub (/bootmii-/, ""); print;}'`
-	else
-		printf "%s" ""`printf "%s" \`git describe --abbrev=4 --match bootmii-\* $comm | awk '{ sub (/bootmii-/, ""); print;}'\``
-	fi
+	printf "%s" ""`printf "%s" \`git describe --abbrev=4 --match bootmii-\* $comm | awk '{ sub (/bootmii-/, ""); print;}'\``
 
 	# Are there uncommitted changes?
 	git update-index --refresh --unmerged > /dev/null
