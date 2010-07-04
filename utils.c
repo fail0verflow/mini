aaba483ab709f0aeaf012f985e8221347e9a0aab
dhewg <dhewg@wiibrew.org>
2010-07-04 17:56:03 +0200

    add missing sprintf(), prototype was already there

--- utils.c
+++ utils.c
@@ -16,6 +16,8 @@ Copyright (C) 2008, 2009	Hector Martin "marcan" <marcan@marcansoft.com>
 #include "gpio.h"
 #include "hollywood.h"
 
+#include <stdarg.h>
+
 #ifndef LOADER
 static char ascii(char s) {
   if(s < 0x20) return '.';
@@ -39,6 +41,17 @@ void hexdump(void *d, int len) {
       else gecko_printf("%c",ascii(data[off+i]));
     gecko_printf("\n");
   }
+}
+
+int sprintf(char *buffer, const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i = vsprintf(buffer, fmt, args);
+	va_end(args);
+	return i;
 }
 #endif
 
